8f995d218b08e18e57ee531861b816d083189920
jacobv1234 <jacobjv1234@gmail.com>
2025-05-29 14:47:26 +0100

    added compiler

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ try:
     import PIL as pillow
     import mariadb as mariadb
     import pytest as pytest
+    import nuitka as nuitka
 except ModuleNotFoundError:
     # automatically install modules
     from os import system
     install = input('Important modules are not installed. Install? (y/n) ')
     if install.lower() == 'y':
-        system('python -m pip install pillow mariadb pytest')
+        system('python -m pip install pillow mariadb pytest nuitka')
         exit()
 
 # global imports
---
+++ Δcompiler.py
@@ -0,0 +1,14 @@
+from os import system
+
+# COMPILER INSTRUCTIONS
+# run this file
+# ignore any yellow warnings
+# delete main.build, main.dist, main.onefile-build folders
+# move EXE and any other files the program uses (except lib folder) into a new folder
+
+# PARAMETERS
+# add --disable-console to hide the terminal in the resulting EXE
+# add --windows-icon-from-ico="filename.ico" to set the icon
+# feel free to change --output-filename
+
+system('python -m nuitka --standalone --onefile --output-filename="Johannesburg Flytipping Report.exe" --enable-plugin=tk-inter main.py')
