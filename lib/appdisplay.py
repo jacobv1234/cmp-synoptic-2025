29b5f25e2130fb22958d19eb0f39aa95c6e963d8
jacobv1234 <jacobjv1234@gmail.com>
2025-05-29 13:55:57 +0100

    moved drawing the front page into its own function

--- lib/appdisplay.py
+++ lib/appdisplay.py
@@ -13,29 +13,47 @@ class AppDisplay:
 
         # Canvas allows for shapes/images to be drawn to the screen + handles user input
         self.c = Canvas(self.window, width=width, height=height, bg='white') 
+        self.c.pack()
+
+        # lists to hold objects drawn to the screen - cobjects is for stuff on the canvas, widgets is for buttons, labels, etc
+        self.cobjects = []
+        self.widgets = []
+
+        # loaded images go in this dictionary so they remain loaded
+        self.images = {}
+
         
+        self.running = True
+
+        self.draw_front_page()
+
+    def draw_front_page(self):
+        self.clear_screen()
+
         textExample = StringVar()
         getDBTestData = str(displayDBData())
         textExample.set(getDBTestData) #a text variable is used in tkinter to dsiplay text here we need to SET the variable
 
         #Here we create the label for the text 
-        self.label = Label(self.window, textvariable=textExample, anchor=CENTER, height=3, width=30, bd=3, padx=15, pady=15, justify=CENTER, relief=RAISED, underline=0, wraplength=250)
-        self.label.place(relx = 0.5, rely=0.5, anchor = CENTER)
+        label = Label(self.window, textvariable=textExample, anchor=CENTER, height=3, width=30, bd=3, padx=15, pady=15, justify=CENTER, relief=RAISED, underline=0, wraplength=250)
+        label.place(relx = 0.5, rely=0.5, anchor = CENTER)
+        self.widgets.append(label)
 
         img = Image.open('images/Enivronment App Logo.png')
         imgSmallerResize = img.resize((360, 480))
-        self.img = ImageTk.PhotoImage(imgSmallerResize)
+        self.images['logo'] = ImageTk.PhotoImage(imgSmallerResize)
 
-        
+        self.cobjects.append(self.c.create_image(250, 150, anchor=CENTER, image=self.images['logo']))
 
-        self.c.create_image(250, 150, anchor=CENTER, image=self.img)
-        
+    def clear_screen(self):
+        for object in self.cobjects:
+            self.c.delete(object)
+        for widget in self.widgets:
+            widget.destroy()
+        self.cobjects = []
+        self.widgets = []
 
-        self.c.pack()
 
-        
-        self.running = True
-    
     # run every frame
     def update(self):
         self.window.update()
