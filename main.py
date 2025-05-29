6ce31f2d73a8bc2d0b09de0eefbf8834529d1e7b
jacobv1234 <jacobjv1234@gmail.com>
2025-05-29 14:37:28 +0100

    auotmatically install python module dependencies

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# third-party imports
+try:
+    import PIL as pillow
+    import mariadb as mariadb
+    import pytest as pytest
+except ModuleNotFoundError:
+    # automatically install modules
+    from os import system
+    install = input('Important modules are not installed. Install? (y/n) ')
+    if install.lower() == 'y':
+        system('python -m pip install pillow mariadb pytest')
+        exit()
+
 # global imports
 from time import sleep, perf_counter
 
