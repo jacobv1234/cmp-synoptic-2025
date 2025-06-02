d1b5f406e165634c57966122dbec01c71d17b3d4
jacobv1234 <jacobjv1234@gmail.com>
2025-06-02 14:46:37 +0100

    added new dependencies to module installer

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ try:
     import mariadb as mariadb
     import pytest as pytest
     import nuitka as nuitka
+    import bcrypt as bcrypt
+    import tkintermapview as tkintermapview
 
 except ModuleNotFoundError:
     # automatically install modules
     from os import system
     install = input('Important modules are not installed. Install? (y/n) ')
     if install.lower() == 'y':
-        system('python -m pip install pillow mariadb pytest nuitka bcrypt ')
+        system('python -m pip install pillow mariadb pytest nuitka bcrypt tkintermapview')
         exit()
 
 # global imports
