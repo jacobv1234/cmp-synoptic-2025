63dfd609045266e3e31c902ec7e89cea0305c11d
Riley Oliver <randymagic241@gmail.com>
2025-06-02 21:47:11 +0100

    fix my stupidity

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
+# third-party imports
+try:
+    import PIL as pillow
+    import mariadb as mariadb
+    import pytest as pytest
+    import nuitka as nuitka
+    import bcrypt as bcrypt
+    import tkintermapview as tkintermapview
+
+except ModuleNotFoundError:
+    # automatically install modules
+    from os import system
+    install = input('Important modules are not installed. Install? (y/n) ')
+    if install.lower() == 'y':
+        system('python -m pip install pillow mariadb pytest nuitka bcrypt tkintermapview')
+        exit()
+
 # global imports
-from time import sleep, perf_counter
+#from time import sleep, perf_counter
 
 # local imports
 from lib.appdisplay import AppDisplay
 
 
 screen = AppDisplay()
+screen.window.mainloop() # run
 
-while screen.running:
-    start = perf_counter()
 
-    screen.update()
+# Alt main loop - scrapped due to not working with the map
 
-    # lock to roughly 60fps
-    end = perf_counter()
-    diff = end - start
-    if diff < 0.017:
-        sleep(0.017 - diff)
+#while screen.running:
+#    start = perf_counter()
+#
+#    screen.update()
+#
+#    # lock to roughly 60fps
+#    end = perf_counter()
+#    difference = end - start
+#    if difference < 0.017:
+#        sleep(0.017 - difference)
     
-
-    
-
